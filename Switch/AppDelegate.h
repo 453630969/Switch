d57ff003e02ca4c65c59ab332a0f3efd36e0b3e8
hezhibo <2465384900@qq.com>
2017-02-27 11:38:30 +0800

    开关控件
    开关控件

---
+++ Switch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Switch
+//
+//  Created by 小何 on 17/2/27.
+//  Copyright © 2017年 zkbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Switch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Switch
+//
+//  Created by 小何 on 17/2/27.
+//  Copyright © 2017年 zkbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
